bbc1f5dce1a4e63181ba5666ebd371a0d07eb0f0
RfqiAlan <rifqialan46475@gmail.com>
2024-11-07 00:07:40 +0700

    TP 9

--- H071241040/Prak_7/qsfiks.py
+++ H071241040/Prak_7/qsfiks.py
@@ -14,6 +14,8 @@ def quick_sort(array):
             right.append(x)
     return quick_sort(left) + middle + quick_sort(right)
 
-array = [1, 8, 20, 7, 9, 11, 13,1,3,2,1,4,3,2,5]
+array = [1, 8, 20, 7, 9, 11, 13, 1, 3, 2, 1, 4, 3, 2, 5]
 sorted_array = quick_sort(array)
 print("Array yang diurutkan:", sorted_array)
+
+
